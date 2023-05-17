ec63598890ec870224ebc0fdd2ee119675dfe711
BenRaz123 <ben.raz2008@gmail.com>
2023-05-17 14:02:41 -0400

    Colors and Rainbows!
    Used the `colored` package to provide a fun rainbow logo.
    
    Signed-off-by: BenRaz123 <ben.raz2008@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
 use clap::*;
+use colored::Colorize;
 use open;
 
 fn main() {
+    let rainbow_google_logo: String = format!(
+        "{}{}{}{}{}{}",
+        "G".bold().blue(),
+        "o".bold().red(),
+        "o".bold().yellow(),
+        "g".bold().blue(),
+        "l".bold().green(),
+        "e".bold().red()
+    );
+
     let my_arguments = Command::new("google")
         .arg(
             Arg::new("query")
@@ -17,7 +28,7 @@ fn main() {
 
     let url: String = format!("https://google.com/search?q={}", query);
 
-    println!("Searching Google For: {}", query);
+    println!("Searching {} For: {}", rainbow_google_logo, query);
 
     open::that(url).expect("Failed to open url")
 }
