cbc4bff7d0ac342d552ed094c3993f3a41183c2f
BenRaz <ben.raz@student.winstonprep.edu>
2023-05-17 13:36:42 -0400

    Housekeeping
    Ran houskeeping including removing some dead code, and generally
    improving it with `cargo fix` and `rustfmt`.
    
    Signed-off-by: BenRaz <ben.raz@student.winstonprep.edu>

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,18 @@ use clap::*;
 use open;
 
 fn main() {
-
     let my_arguments = Command::new("google")
-        .arg(Arg::new("query").help("A query to Google Search").required(true))
+        .arg(
+            Arg::new("query")
+                .help("A query to Google Search")
+                .required(true),
+        )
         .get_matches();
 
-    let mut query: String = my_arguments.get_one::<String>("query").expect("Could not parse argument").to_owned();
+    let query: String = my_arguments
+        .get_one::<String>("query")
+        .expect("Could not parse argument")
+        .to_owned();
 
     let url: String = format!("https://google.com/search?q={}", query);
 
